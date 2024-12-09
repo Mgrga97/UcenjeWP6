f4b47e0c429909af44e75ffaa0bbb6b2074e3b4d
Mgrga97 <marko.grg97@gmail.com>
2024-12-09 18:56:16 +0100

    dzzzzz

---
+++ CSHARP/Ucenje/E07Z2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E07Z2
+    {
+
+        // za dva unesena cijela broja ispisati sve brojeve izmedu
+
+        // DZ rjesiti program gdje je prvi broj veci od drugog i ispise brojeve 
+        public static void Izvedi()
+        {
+            Console.WriteLine("Unesite prvi broj: ");
+            int prvibroj = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite drugi broj: ");
+            int drugibroj = int.Parse(Console.ReadLine());
+
+            for (int i = prvibroj; i < drugibroj; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        
+    }
+}
--- CSHARP/Ucenje/Program.cs
+++ CSHARP/Ucenje/Program.cs
@@ -2,7 +2,11 @@
 
 using Ucenje;
 
-E07ForPetlja.Izvedi();
+E07Z2.Izvedi();
+
+// E07Z1.Izvedi();
+
+// E07ForPetlja.Izvedi();
 
 // E04Z4.Izvedi();
 
