fcdbc0fbac06b18de387b05794486b434e8c3e78
Mgrga97 <marko.grg97@gmail.com>
2024-12-18 18:57:52 +0100

    dodat na e13z2 rjesenje dz

--- CSHARP/Ucenje/E12Metode.cs
+++ CSHARP/Ucenje/E12Metode.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -112,6 +113,24 @@ namespace Ucenje
 
            //  return 0; // kasnije obrisemo
 
+        }
+
+        public static string UcitajString(string poruka)
+        {
+            string s = "";
+            while (true)
+            {
+                Console.Write(poruka);
+                s = Console.ReadLine().Trim();
+                if (s.Length == 0)
+                {
+                    Console.WriteLine("Obavezan unos");
+                    continue;
+                }
+                return s;
+            }
+
+           // return "";
         }
 
 
