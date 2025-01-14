730dd87d672f933a1db65b9aea1832142d2afb82
Mgrga97 <marko.grg97@gmail.com>
2025-01-15 00:24:11 +0100

    Folder za završni rad

---
+++ CSHARP/Ucenje/E17KlasaObjekt/ZavrsniRad/Korisnik.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje.E17KlasaObjekt.ZavrsniRad
+{
+    public class Korisnik
+    {
+        public int sifra { get; set; }
+
+        public string Ime { get; set; } = "";
+
+        public string Prezime { get; set; } = "";
+
+        public string Email { get; set; } = "";
+
+        public string? Lozinka { get; set; }
+
+
+
+    }
+}
