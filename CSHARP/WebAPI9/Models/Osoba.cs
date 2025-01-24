30304bf76c79ad961342187dc94b816742ee7650
Mgrga97 <marko.grg97@gmail.com>
2025-01-24 18:53:01 +0100

    Web API uvod

---
+++ CSHARP/WebAPI9/Models/Osoba.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPI9.Models
+{
+    public class Osoba
+    {
+        public int Sifra { get; set; }
+        public string? Ime { get; set; }
+        public decimal Koeficijent { get; set; }
+        public DateTime? DatumRodenja { get; set; }
+        public bool Aktivan { get; set; }
+
+
+    }
+}
