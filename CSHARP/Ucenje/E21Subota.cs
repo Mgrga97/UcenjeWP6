fc537379b0d483f62452dc996b72a9e22a075167
Mgrga97 <marko.grg97@gmail.com>
2025-01-25 10:57:13 +0100

    pomoć za ljubavni kalkulator

--- CSHARP/Ucenje/E21Subota.cs
+++ CSHARP/Ucenje/E21Subota.cs
@@ -12,6 +12,52 @@ namespace Ucenje
         {
             Console.WriteLine("Hello from E21 Subota");
             SlucajniBrojevi();
+            SlucajniDatumiRodenja();
+            ljubav();
+
+        }
+
+        private void ljubav()
+        {
+            var ona = "Marta";
+            var on = "Manuel";
+
+            var izraz = ona.Trim().ToLower() + on.Trim().ToLower();
+
+            Console.WriteLine(izraz);
+
+            int[] brojevi = new int[izraz.Length];
+            var ponovilose = 0;
+            for (int i = 0; i < izraz.Length; i++)
+            {
+                for (int j = 0; j < izraz.Length; j++)
+                {
+                    if (izraz[i] == izraz[j])
+                    {
+                        brojevi[i]++;
+                    }
+                }
+                
+            }
+
+
+        }
+
+        private void SlucajniDatumiRodenja()
+        {
+            var random = new Random();
+
+            for (int i = 0; i < 100; i++)
+                try
+                {
+                    var d = new DateTime(2023, random.Next(1, 13), random.Next(1, 32));
+                    Console.WriteLine((i+1) + ": " + d.ToString("yyyy-MM-dd"));
+                }
+                catch
+                {
+                    i--;
+
+                }
 
         }
 
