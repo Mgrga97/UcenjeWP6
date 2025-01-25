df87fcc8e3c83ff790feb8c63bee835e96956c35
Mgrga97 <marko.grg97@gmail.com>
2025-01-25 10:27:53 +0100

    Konzolna aplikacija,slučajni brojevi

---
+++ CSHARP/Ucenje/E21Subota.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    class E21Subota
+    {
+        public E21Subota()
+        {
+            Console.WriteLine("Hello from E21 Subota");
+            SlucajniBrojevi();
+
+        }
+
+        private void SlucajniBrojevi()
+        {
+            var random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(random.Next(20, 31));
+            }
+        }
+    }
+}
