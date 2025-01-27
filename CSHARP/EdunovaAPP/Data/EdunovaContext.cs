ec52171bad09f86857c7fdbec318f811fea32b10
Mgrga97 <marko.grg97@gmail.com>
2025-01-27 18:01:06 +0100

    Instalacije itd,priprema za smjer Read

---
+++ CSHARP/EdunovaAPP/Data/EdunovaContext.cs
@@ -0,0 +1,20 @@
+﻿using EdunovaAPP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EdunovaAPP.Data
+{
+    public class EdunovaContext : DbContext 
+    {
+        public EdunovaContext(DbContextOptions<EdunovaContext> options) : base(options)
+        {
+            // ovdje bi upravljali s razlicitim opcijama
+
+        }
+
+        public DbSet<Smjer> Smjerovi { get; set; }
+
+
+    }
+    
+    
+}
---
+++ CSHARP/EdunovaAPP/Models/Smjer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EdunovaAPP.Models
+{
+    public class Smjer : Entitet
+    {
+        public string Naziv { get; set; } = "";
+        [Column("Cijena")] // ovo je bit entity frameworka, mogucnost mapiranja
+        public decimal? CijenaSmjera { get; set; }
+        public DateTime IzvodiSeOd { get; set; }
+        public bool Vaucer { get; set; }
+    }
+}
